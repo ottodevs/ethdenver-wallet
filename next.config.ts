a1687bce0524410ef42d884003f39baaa2b3bea1
Otto G <5030059+ottodevs@users.noreply.github.com>
2025-03-28 23:05:05 +0100

    🔧 chore(config): add content security policy and update headers in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -9,9 +9,23 @@ const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
 
 const NEXTAUTH_URL = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : process.env.NEXTAUTH_URL
 
-console.log('NEXT_PUBLIC_SERVER_URL', NEXT_PUBLIC_SERVER_URL)
+const contentSecurityPolicy = `
+    default-src 'self';
+    connect-src 'self' https://accounts.google.com https://*.okto.tech https://*.oktostage.com;
+    frame-src 'self' https://accounts.google.com;
+    frame-ancestors 'self';
+    script-src 'self' https://accounts.google.com 'unsafe-inline' 'unsafe-eval';
+    script-src-elem 'self' https://accounts.google.com 'unsafe-inline';
+    worker-src 'self' blob:;
+    style-src 'self' https://accounts.google.com 'unsafe-inline';
+    img-src 'self' https://lh3.googleusercontent.com https://s2.coinmarketcap.com data: blob:;
+    object-src 'self' data: blob:;
+`
+    .replace(/\s{2,}/g, ' ')
+    .trim()
 
 const nextConfig: NextConfig = {
+    devIndicators: false,
     env: {
         NEXTAUTH_URL,
         NEXT_PUBLIC_SERVER_URL,
@@ -47,11 +61,19 @@ const nextConfig: NextConfig = {
                         key: 'Cross-Origin-Resource-Policy',
                         value: 'same-site',
                     },
+                    {
+                        key: 'Permissions-Policy',
+                        value: 'identity-credentials-get=*',
+                    },
+                    {
+                        key: 'Content-Security-Policy',
+                        value: contentSecurityPolicy,
+                    },
                 ],
             },
         ]
     },
-    // reactStrictMode: true,
+    reactStrictMode: true,
 }
 
 export default nextConfig
