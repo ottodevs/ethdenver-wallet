def9d412935ff3bc614b5a9d3348e9b19d9db492
Otto G <5030059+ottodevs@users.noreply.github.com>
2025-03-28 23:09:00 +0100

    🔧 chore(middleware): refactor authentication middleware to use new auth configuration

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,47 +1,4 @@
-import { auth } from '@/features/auth/config'
-import type { NextRequest } from 'next/server'
-import { NextResponse } from 'next/server'
-
-export default auth(req => {
-    // Do not apply authentication to static resources
-    const isStaticResource =
-        req.nextUrl.pathname.startsWith('/_next') ||
-        req.nextUrl.pathname.includes('.svg') ||
-        req.nextUrl.pathname.includes('.png') ||
-        req.nextUrl.pathname.includes('.jpg') ||
-        req.nextUrl.pathname.includes('.jpeg')
-
-    if (isStaticResource) {
-        return NextResponse.next()
-    }
-
-    // Allow access to the authentication page without being authenticated
-    if (req.nextUrl.pathname === '/auth') {
-        return NextResponse.next()
-    }
-
-    // Redirect to login if not authenticated and not a public route
-    if (!req.auth && !req.nextUrl.pathname.startsWith('/api/auth')) {
-        const newUrl = new URL('/auth', req.nextUrl.origin)
-        return Response.redirect(newUrl)
-    }
-
-    // Configure CORS headers
-    const response = NextResponse.next()
-    response.headers.set('Access-Control-Allow-Credentials', 'true')
-    response.headers.set('Access-Control-Allow-Origin', '*')
-    response.headers.set('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
-    response.headers.set(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-    )
-
-    return response
-})
-
-export function middleware(_request: NextRequest) {
-    return NextResponse.next()
-}
+export { auth as middleware } from '@/features/auth/auth.config'
 
 export const config = {
     matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
